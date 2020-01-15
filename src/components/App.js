19ccd3986fb71aade40026adac225ead1afbcb73
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2020-01-15 16:14:04 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 import NavBar from './NavBar'
 
-// the App component should render out the GifListContainer component 
+"data": [
+    {
+      "type": "gif",
+      "id": "l0HlNQ03J5JxX6lva",
+      "slug": "bbc-wildlife-l0HlNQ03J5JxX6lva",
+      "url": "https://giphy.com/gifs/bbc-wildlife-l0HlNQ03J5JxX6lva",
+      "bitly_gif_url": "https://gph.is/2iC32M8",
+      "bitly_url": "https://gph.is/2iC32M8",
+ 
+      
+ 
+      "images": {
+        "fixed_height_still": {
+          "url": "https://media0.giphy.com/media/l0HlNQ03J5JxX6lva/200_s.gif?cid=e1bb72ff5b9fa2866168584b51f13892",
+          "width": "400",
+          "height": "200",
+          "size": "55556"
+        },
+        
+        "original": {
+          "url": "https://media0.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif?cid=e1bb72ff5b9fa2866168584b51f13892",
+          "width": "480",
+          "height": "240",
+        }
+      
+      }
+    }
+    ]
 
 const App = () => {
   return (
